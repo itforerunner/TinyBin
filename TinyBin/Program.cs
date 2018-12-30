647eba871c7f8b49b981bfd7150ff092c581a4b4
robin <xuebin_li@outlook.com>
2018-12-30 15:58:27 +0800

    添加项目文件。

---
+++ TinyBin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TinyBin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
